27e5b15d55fd2ae0f5d7771c5e3aa7996bbe6eeb
Keshav Gainda <keshavgainda@Keshavs-MacBook-Pro.local>
2025-04-17 14:26:06 -0400

    updated home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home'
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path = "/Home" Component={Home} />
+        <Route path="/" Component={Home} />
       </Routes>
     </Router>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react'
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+import Icon from '@mdi/react';
+import { mdiCubeOutline } from '@mdi/js';
 
-const Home = () => {
-    return(
-        <h1>Hello from home</h1>
-    );
-    
-};
+
+const Home = () => (
+    <Fragment>
+        <Helmet><title>Quiz Application</title></Helmet>
+        <div id='home'>
+            <section>
+                <div style={{textAlign : 'center'}}>
+                    <span className="cube"><Icon path={mdiCubeOutline} size={4} /></span>
+                </div>
+                <h1>Quiz Application</h1>
+                <div className='play-button-container'>
+                    <ul>
+                        <li><Link  className='play-button' to="/play/instructions">Play</Link></li>
+                    </ul>
+                </div>
+                <div className='auth-container'>
+                    <Link to="/login" className='auth-buttons' id='login-button'>Login</Link>
+                    <Link to="/register" className='auth-buttons' id='register-button'>Register</Link>
+                </div>
+            </section>
+        </div>
+    </Fragment>
+)
 
 export default Home;
